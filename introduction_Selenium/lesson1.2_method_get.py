b0749332528193bf6654ec240e24569b342defd1
Alexandr Plakhotnikov <mxplaha@gmail.com>
2020-02-21 16:20:53 +0300

    example script method get webdriver

---
+++ introduction_Selenium/lesson1.2_method_get.py
@@ -0,0 +1,16 @@
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+time.sleep(5)
+driver.get("https://stepik.org/lesson/25969/step/12")
+time.sleep(5)
+textarea = driver.find_element_by_css_selector(".textarea")
+textarea.send_keys("get()")
+time.sleep(5)
+submit_button = driver.find_element_by_css_selector(".submit-submission")
+driver.execute_script("window.scrollBy(0, 200)")
+time.sleep(2)
+submit_button.click()
+time.sleep(5)
+driver.quit()
