ad7fea73cf50853376812e121d1851888e89bd5a
Alexandr Plakhotnikov <mxplaha@gmail.com>
2020-02-28 16:44:16 +0300

    refactoring file lesson3.2_13_test_in_style_unittest.py

--- test_frameworks/lesson3.2_13_test_in_style_unittest.py
+++ test_frameworks/lesson3.2_13_test_in_style_unittest.py
@@ -6,12 +6,12 @@ class TestUniqueSelectors(unittest.TestCase):
 
     def setUp(self) -> None:
         self.browser = webdriver.Chrome()
+        self.browser.maximize_window()
+        self.browser.implicitly_wait(5)
 
     def test_unique_selector1(self):
         page_url1 = 'http://suninjuly.github.io/registration1.html'
         browser = self.browser
-        browser.implicitly_wait(5)
-        browser.maximize_window()
         browser.get(page_url1)
         # first name
         first_name = browser.find_element_by_xpath("//input[@class='form-control first' and @required]")
@@ -22,18 +22,16 @@ class TestUniqueSelectors(unittest.TestCase):
         # email
         email = browser.find_element_by_class_name("third")
         email.send_keys("123@mail.ru")
-        # clicl button submit
+        # click button submit
         browser.find_element_by_css_selector("button.btn").click()
         welcome_text_elt = browser.find_element_by_tag_name("h1")
         welcome_text = welcome_text_elt.text
         self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
 
     def test_unique_selector2(self):
-        page_url1 = 'http://suninjuly.github.io/registration2.html'
+        page_url2 = 'http://suninjuly.github.io/registration2.html'
         browser = self.browser
-        browser.implicitly_wait(5)
-        browser.maximize_window()
-        browser.get(page_url1)
+        browser.get(page_url2)
         # first name
         first_name = browser.find_element_by_xpath("//input[@class='form-control first' and @required]")
         first_name.send_keys("Alexandro")
