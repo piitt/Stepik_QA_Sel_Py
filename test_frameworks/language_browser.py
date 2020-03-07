61e2cc5d0699c7fc368e44f2be57f8247674d251
Alexandr Plakhotnikov <mxplaha@gmail.com>
2020-03-07 12:49:41 +0300

    lesson 3.6 step 8 set language for webdriver

---
+++ test_frameworks/language_browser.py
@@ -0,0 +1,11 @@
+# set language browser in WebDriver
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+browser = webdriver.Chrome(options=options)
+
+fp = webdriver.FirefoxProfile()
+fp.set_preference("intl.accept_languages", user_language)
+browser = webdriver.Firefox(firefox_profile=fp)
